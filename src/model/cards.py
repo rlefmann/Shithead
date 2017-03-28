f4e9fa50b76306595c3aebd148aec4cf9cff3bcf
Raoul Lefmann <raoul.lefmann@gmail.com>
2017-03-28 13:27:57 +0200

    started implementing cardcollection

--- src/model/cards.py
+++ src/model/cards.py
@@ -30,3 +30,66 @@ class Card:
 		the player who begins the game.
 		"""
 		return self.rank*4 + self.suit
+    
+    
+class CardCollection:
+    """
+    A collection of zero or more cards. This is the base class for specific
+    collections like hands, stacks, or the deck of the game.
+    
+    Attributes:
+        cards (list of Card): This is where the cards are stored
+        hidden (boolean): Determines wether the cards are visible to the
+            player or not. Influences for example what happens, if you
+            print the cards
+    """
+    def __init__(self, hidden = False):
+        self.cards = []
+        self.hidden = hidden
+        
+    def add(self, cards):
+        """
+        Adds more cards to the collection.
+        
+        Args:
+            cards (list of Card): the new cards, that should be added.
+        """
+        self.cards.extend(cards)
+        
+    def remove(self, indices):
+        """
+        Removes the cards at the specified indices of the collection.
+        
+        Args:
+            indices (list of Int): the indices of cards to be removed.
+            
+        Returns:
+            A list of the removed cards.
+        """
+        pass # TODO: implement
+    
+    def delete(self, idx):
+        """
+        Deletes the card at the specified index.
+        
+        Args:
+            idx (Int): the index of the card that should be deleted.
+        """
+        del self.cards[idx]
+        
+    def __len__(self):
+        """
+        The number of cards in the CardCollection is simply the length
+        of self.cards.
+        """
+        return len(self.cards)
+    
+    
+    class Hand(CardCollection):
+        """
+        The hand of a player. This extends the CardCollection by being
+        sortable (to more easily select cards) and having a nice string
+        representation, which depends on whether the hand's visible
+        attribute is set or not.
+        """
+        pass
