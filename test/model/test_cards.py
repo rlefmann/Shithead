809307908974d9f50f58b3655dfc8a6417bde8df
Raoul Lefmann <raoul.lefmann@gmail.com>
2017-04-04 10:20:53 +0200

    added test for cardcollection class

--- test/model/test_cards.py
+++ test/model/test_cards.py
@@ -37,11 +37,24 @@ class CardCollectionTest(unittest.TestCase):
         self.assertRaises(TypeError, self.cc.add, self.c1)
     
     def test_cc_2(self):
-        """
-        Tests removing cards from the collection.
-        """
-        self.cc.add([self.c1, self.c2, self.c3])
-        self.assertRaises(ValueError, self.cc.add, self.c1)
-    
+		"""
+		Tests removing cards from the collection.
+		"""
+		self.cc.add([self.c1, self.c2, self.c3, self.c3, self.c2, self.c1])
+		# you must remove a list or range of indices:
+		self.assertRaises(TypeError, self.cc.remove, self.c1)
+		self.assertRaises(TypeError, self.cc.remove, 0)
+		# you have to remove at least one index:
+		self.assertRaises(ValueError, self.cc.remove, [])
+		# removing a range:
+		self.cc.remove(range(3))
+		self.assertEqual(len(self.cc), 3)
+		self.assertEqual(self.cc.cards[0].rank, 8)
+		# now remove list of indices:
+		self.cc.remove([1,2])
+		self.assertEqual(len(self.cc), 1)
+		self.assertEqual(self.cc.cards[0].rank, 8)
+		
+		
 if __name__ == "__main__":
     unittest.main(exit=False)
