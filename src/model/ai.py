ae16a278e8c8907025bda71c50bee13231e780a2
Raoul Lefmann <raoul.lefmann@gmail.com>
2017-05-08 13:55:55 +0200

    updated ai class so that it works again

--- src/model/ai.py
+++ src/model/ai.py
@@ -22,37 +22,56 @@ class StraightforwardAI(AI):
 		super(StraightforwardAI, self).__init__(game)
 
 	def think(self):
-		if self.game._curplayer == 0:
+		if self.game._curplayer == 0: # change to not access local var
 			raise Exception("the computer player is always player 1, but it is player 0s turn")
-		src_collection, smallestplayable = self.findsmallestplayableindices()
-		if len(smallestplayable) == 0:
-			return RequestTake()
+		if self.game.mode == GameMode.DOWNCARDS:
+			playsrc = self.game.curupcards
+			# return the first downcard slot that is not empty:
+			i = 0
+			while i < len(playsrc) and playsrc[i] is None:
+				i+=1
+			if i == len(playsrc):
+				raise Exception("all downcards were already played")
+			return RequestMove.play_from_downcards([i])
 		else:
-			return RequestPlay(AType.PLAY, smallestplayable)
+			src_collection, smallestplayable = self.findsmallestplayableindices()
+			if len(smallestplayable) == 0:
+				return RequestMove.take()
+			else:
+				if self.game.mode == GameMode.HAND:
+					return RequestMove.play_from_hand(smallestplayable)
+				elif self.game.mode == GameMode.UPCARDS:
+					return RequestMove.play_from_upcards(smallestplayable)
 		
 	def findsmallestplayableindices(self):
 		"""
 		Finds the index of the smallest playable card, or indices,
 		if there are several cards with the same rank
 		"""
-		src_coll = self.game.curplayer # TODO: here: get collection depending on gamemode
-		pcards = self.game.players[1].hand
+		 # TODO: here: get collection depending on gamemode
+		if self.game.mode == GameMode.HAND:
+			src_coll = self.game.curhand
+		elif self.game.mode == GameMode.UPCARDS:
+			src_coll = self.game.curupcards
+		elif self.game.mode == GameMode.DOWNCARDS:
+			raise Exception("we cannot find the smallest playable index from downcards")
 
-		# create a order which is different 
-		order = []
+		# create an order of the ranks:
 		special = [self.game.settings["INVISIBLE"], self.game.settings["BURN"]]
+		#regular = range(13)
+		#for s in special:
+		#	regular.remove(s)
 		if self.game.minval == self.game.settings["LOWER"]:
-			r = range(self.game.settings["LOWER"]+1)
+			r = range(self.game.settings["LOWER"]+1) # 0,1,...,minval
 		else:
-			r = range(self.game.minval,13)
-		for i in r:
-			if i not in special:
-				order.append(i)
+			r = range(self.game.minval,13) # minval,minval+1,...,12
+		order = [i for i in r if i not in special]
 		order.extend(special) # special cards are last
 
 		indices = []
 		for rank in order:
-			indices = [i for i,x in enumerate(pcards) if x.rank==rank]
+			indices = [i for i,x in enumerate(src_coll) if x is not None and x.rank==rank]
+			# the src_coll contains cards of this rank:
 			if len(indices)>0:
 				break
-		return indices
+		return src_coll, indices
